b53c7ff8ac116205ef48f093897d8d45fad661d4
anotherMonogatari <azaza270898@gmail.com>
2020-03-18 14:38:19 +0200

    initial

---
+++ precache-manifest.142d134aa9720304d58bb6cdb7520e91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c8d4c3c5bdd4289a7adedfe37e073e4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c49a67db4d0d43c89af5",
+    "url": "./static/css/2.83ace7dd.chunk.css"
+  },
+  {
+    "revision": "c49a67db4d0d43c89af5",
+    "url": "./static/js/2.779a79e5.chunk.js"
+  },
+  {
+    "revision": "b018791ce71469f774cb",
+    "url": "./static/js/main.6da1d878.chunk.js"
+  },
+  {
+    "revision": "d6c510366f687c741b6f",
+    "url": "./static/js/runtime-main.f73e1d02.js"
+  }
+]);
